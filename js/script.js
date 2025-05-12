d5034975d6fe64c8a09dddcbcd64a0c2efcd6ee8
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-12 17:00:07 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Shanea Jaromay
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
