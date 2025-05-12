ba06329d005bf88301a68a6b483576c2cfb252b5
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-12 17:03:29 +0000

    ok

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
-// Copyright (c) 2020 Shanea Jaromay All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
 // Created by: Shanea Jaromay
 // Created on: May 2025
+
 // This file contains the JS functions for index.html
 
-"use strict"
+"use-strict"
 
 /**
- * Check servie worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
-  })
-}
+* This function calculate, doing multiplication
+*/
+// eslint-disable-next-line no-unused-vars
+function calculateNumber () {
+  //input 
+  let counter = 0
+  let numberOne = parseFloat(document.getElementById('number-one').value)
+  const numberTwo = parseFloat(document.getElementById('number-two').value)
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  //process
+  while (numberOne >= numberTwo ) {
+    counter ++
+    numberOne -= numberTwo
+
+    if (numberOne < numberTwo){
+      break
+    }
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = 'Your answer is:' + counter + ' R ' + numberOne
 }
